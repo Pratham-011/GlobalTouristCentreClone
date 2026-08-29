06d933ad9bf2fb8d2bc780cf77cc433be54011bd
jff2009 <jaredfurtadowork@gmail.com>
2026-08-29 14:27:06 +0530

    data is changed to json format

--- lib/i18n/translations/de/index.ts
+++ lib/i18n/translations/de/index.ts
@@ -1,11 +1,11 @@
-import { common } from './common';
-import { pages } from './pages';
-import { services } from './services';
-import { destinations } from './destinations';
-import { blogs } from './blogs';
-import { luxury } from './luxury';
-import { trips } from './trips';
-import { tourData } from './tourData';
+import common from './common.json';
+import pages from './pages.json';
+import services from './services.json';
+import destinations from './destinations.json';
+import blogs from './blogs.json';
+import luxury from './luxury.json';
+import trips from './trips.json';
+import tourData from './tourData.json';
 
 export const de = {
   ...common,
